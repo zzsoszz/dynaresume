fa6db4c6a0422fe1ce56891cdb760e721d75eeb6
pascal.leclercq@gmail.com <pascal.leclercq@gmail.com@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2009-12-05 09:13:24 +0000

    Java Header files updated

--- bundles-ui/org.dynaresume.common.simpleclient/src/main/java/org/dynaresume/Activator.java
+++ bundles-ui/org.dynaresume.common.simpleclient/src/main/java/org/dynaresume/Activator.java
@@ -1,15 +1,16 @@
 /*****************************************************************************
-* Copyright (c) 2009
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*     Angelo Zerr <angelo.zerr@gmail.com>
-*     Pascal Leclercq <pascal.leclercq@gmail.com>
-*******************************************************************************/
-
+ * Copyright (c) 2009
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Angelo Zerr <angelo.zerr@gmail.com>
+ *     Jawher Moussa <jawher.moussa@gmail.com>
+ *     Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
+ *     Pascal Leclercq <pascal.leclercq@gmail.com>
+ *******************************************************************************/
 package org.dynaresume;
 
 import org.eclipse.core.runtime.Platform;
