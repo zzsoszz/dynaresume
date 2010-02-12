953962d2c897324b1a6696b0d35a730d0405c6db
pascal.leclercq@gmail.com <pascal.leclercq@gmail.com@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-02-12 08:23:01 +0000

    Blueprint Impl based on DM 2.0M1

---
+++ branches/blueprint_DM2.0M1/bundles-ui/org.dynaresume.common.simpleclient/src/main/java/org/dynaresume/Activator.java
@@ -0,0 +1,83 @@
+/*****************************************************************************
+ * Copyright (c) 2009
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Angelo Zerr <angelo.zerr@gmail.com>
+ *     Jawher Moussa <jawher.moussa@gmail.com>
+ *     Nicolas Inchauspe <nicolas.inchauspe@gmail.com>
+ *     Pascal Leclercq <pascal.leclercq@gmail.com>
+ *******************************************************************************/
+package org.dynaresume;
+
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.demo.simpleclient";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+		try{
+		Bundle springDM = Platform.getBundle("org.springframework.osgi.extender");
+
+		springDM.start();
+		} catch (Throwable t){
+			t.printStackTrace();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+}
