d8adbfa5442a8e13e88b83a458d940279a8ced1a
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-04-30 15:58:20 +0000

    Initial Import.

---
+++ trunk/SpringWeaver/incubation/org.eclipse.equinox.weaving.springweaver_0.1.3/src/org/eclipse/equinox/weaving/springweaver/EquinoxAspectsLoadTimeWeaver.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Martin Lippert and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Martin Lippert                   initial implementation      
+ *   Angelo ZERR                      manage springweaver into Jpa context
+ *******************************************************************************/
+package org.eclipse.equinox.weaving.springweaver;
+
+import java.lang.instrument.ClassFileTransformer;
+
+import org.eclipse.equinox.service.weaving.IWeavingService;
+import org.eclipse.equinox.service.weaving.IWeavingServiceFactory;
+import org.osgi.framework.BundleContext;
+import org.springframework.instrument.classloading.LoadTimeWeaver;
+import org.springframework.instrument.classloading.SimpleThrowawayClassLoader;
+import org.springframework.osgi.context.BundleContextAware;
+import org.springframework.util.ClassUtils;
+
+/**
+ * Spring {@link LoadTimeWeaver} implementation for Equinox by using
+ * {@link IWeavingServiceFactory} and {@link IWeavingService} from
+ * Equinox-Aspects.
+ * 
+ * Ex :
+ * 
+ * <code><bean	class="org.eclipse.equinox.weaving.springweaver.EquinoxAspectsLoadTimeWeaver">
+	property name="weaverScope" value="APPLICATION" />
+	</bean>
+	</code>
+ */
+public class EquinoxAspectsLoadTimeWeaver implements LoadTimeWeaver,
+		BundleContextAware {
+
+	private WeaverScope weaverScope = WeaverScope.BUNDLE;
+	private String importDynamicPackages;
+	
+	private final ClassLoader classLoader;
+	private BundleContext bundleContext;
+
+	public EquinoxAspectsLoadTimeWeaver() {
+		this(ClassUtils.getDefaultClassLoader());
+	}
+
+	public EquinoxAspectsLoadTimeWeaver(ClassLoader classLoader) {
+		this.classLoader = classLoader;
+	}
+
+	public void setWeaverScope(WeaverScope weaverScope) {
+		this.weaverScope = weaverScope;
+	}
+	
+	public void setImportDynamicPackages(String importDynamicPackages) {
+		this.importDynamicPackages = importDynamicPackages;
+	}
+
+	@Override
+	public void addTransformer(ClassFileTransformer transformer) {
+		Activator.getInstance().getTransformerRegistry().addTransformer( 
+				this.weaverScope, this.bundleContext.getBundle(), transformer, this.importDynamicPackages);
+		System.out.println("transformer added; " + transformer);
+	}
+
+	@Override
+	public ClassLoader getInstrumentableClassLoader() {
+		return this.classLoader;
+	}
+
+	@Override
+	public ClassLoader getThrowawayClassLoader() {
+		return new SimpleThrowawayClassLoader(getInstrumentableClassLoader());
+	}
+
+	@Override
+	public void setBundleContext(BundleContext bundleContext) {
+		this.bundleContext = bundleContext;
+	}
+
+}
---
+++ trunk/SpringWeaver/incubation/org.eclipse.equinox.weaving.springweaver_0.1.3/src/org/eclipse/equinox/weaving/springweaver/WeavingService.java
@@ -0,0 +1,139 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Martin Lippert and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Martin Lippert                   initial implementation      
+ *******************************************************************************/
+package org.eclipse.equinox.weaving.springweaver;
+
+import java.io.IOException;
+import java.lang.instrument.IllegalClassFormatException;
+import java.util.Map;
+
+import org.eclipse.equinox.service.weaving.IWeavingService;
+import org.eclipse.osgi.internal.baseadaptor.DefaultClassLoader;
+import org.eclipse.osgi.internal.composite.CompositeClassLoader;
+import org.eclipse.osgi.internal.loader.BundleLoader;
+import org.eclipse.osgi.util.ManifestElement;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
+
+/**
+ * Springweaver {@link IWeavingService} implementation.
+ * 
+ */
+public class WeavingService implements IWeavingService {
+
+	private final ClassFileTransformerRegistry registry;
+	private final Bundle bundle;
+	/**
+	 * true if one class of the bundle was woven and false otherwise.
+	 */
+	private boolean oneClassWasWoven = false;
+
+	public WeavingService(final Bundle bundle,
+			ClassFileTransformerRegistry registry) {
+		this.bundle = bundle;
+		this.registry = registry;
+	}
+
+	/**
+	 * @see org.eclipse.equinox.service.weaving.IWeavingService#flushGeneratedClasses(java.lang.ClassLoader)
+	 */
+	public void flushGeneratedClasses(final ClassLoader loader) {
+	}
+
+	/**
+	 * @see org.eclipse.equinox.service.weaving.IWeavingService#generatedClassesExistFor(java.lang.ClassLoader,
+	 *      java.lang.String)
+	 */
+	public boolean generatedClassesExistFor(final ClassLoader loader,
+			final String className) {
+		return false;
+	}
+
+	/**
+	 * @see org.eclipse.equinox.service.weaving.IWeavingService#getGeneratedClassesFor(java.lang.String)
+	 */
+	public Map<String, byte[]> getGeneratedClassesFor(final String className) {
+		return null;
+	}
+
+	/**
+	 * @see org.eclipse.equinox.service.weaving.IWeavingService#getKey()
+	 */
+	public String getKey() {
+		return "spring";
+	}
+
+	/**
+	 * @see org.eclipse.equinox.service.weaving.IWeavingService#preProcess(java.lang.String,
+	 *      byte[], java.lang.ClassLoader)
+	 */
+	public byte[] preProcess(final String name, final byte[] classbytes,
+			final ClassLoader loader) throws IOException {
+
+		byte[] result = null;
+
+		ClassFileTransformerWrapper transformer = null;
+		ClassFileTransformerWrapper[] transformers = this.registry
+				.getTransformers(this.bundle);
+		for (int i = 0; i < transformers.length; i++) {
+			try {
+				transformer = transformers[i];
+				result = transformer.transform(loader, name, null, null,
+						classbytes);
+				importDynamicPackagesIfNeeded(transformer, result, loader);
+			} catch (IllegalClassFormatException e) {
+				e.printStackTrace();
+			}
+		}
+		if (result != null) {
+			oneClassWasWoven = true;
+		}
+		return result;
+	}
+
+	private void importDynamicPackagesIfNeeded(
+			final ClassFileTransformerWrapper transformer,
+			final byte[] newBytes, final ClassLoader loader) {
+
+		if (newBytes == null)
+			return;
+		
+		if (oneClassWasWoven)
+			return;
+		String packages = transformer.getImportDynamicPackages();
+		if (packages == null && packages.length() < 1)
+			return;
+
+		BundleLoader bundleLoader = getBundleLoader(loader);
+		if (bundleLoader != null) {
+			try {
+				bundleLoader
+						.addDynamicImportPackage(ManifestElement.parseHeader(
+								Constants.DYNAMICIMPORT_PACKAGE, packages));
+			} catch (BundleException e) {
+				// TODO : which exception to throw????
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private BundleLoader getBundleLoader(ClassLoader loader) {
+		if (loader instanceof DefaultClassLoader) {
+			return (BundleLoader) ((DefaultClassLoader) loader).getDelegate();
+		}
+		if (loader instanceof CompositeClassLoader) {
+			return (BundleLoader) ((CompositeClassLoader) loader).getDelegate();
+		}
+		return null;
+	}
+
+}
