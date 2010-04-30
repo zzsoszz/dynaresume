a77dbae4c09eb58fa5eeff117f8c6eb08168d3be
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-04-30 19:05:54 +0000

    Add comments.

--- SpringWeaver/incubation/org.eclipse.equinox.weaving.springweaver_0.1.3/src/org/eclipse/equinox/weaving/springweaver/ClassFileTransformerWrapper.java
+++ SpringWeaver/incubation/org.eclipse.equinox.weaving.springweaver_0.1.3/src/org/eclipse/equinox/weaving/springweaver/ClassFileTransformerWrapper.java
@@ -1,32 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Martin Lippert and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *   Martin Lippert                   initial implementation      
+ *   Angelo ZERR                      manage springweaver into Jpa context
+ *******************************************************************************/
 package org.eclipse.equinox.weaving.springweaver;
 
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
 
+import org.osgi.framework.Bundle;
+
+/**
+ * Wrapper of {@link ClassFileTransformer} wich contains dynamic packages wich
+ * must be imported into the {@link Bundle} when transformation is done.
+ * 
+ */
 public class ClassFileTransformerWrapper implements ClassFileTransformer {
-	
+
 	private ClassFileTransformer classFileTransformer;
 	private String importDynamicPackages;
 
 	public ClassFileTransformerWrapper(
-		ClassFileTransformer classFileTransformer,
-		String importDynamicPackages) {
-	    this.classFileTransformer = classFileTransformer;
-	    this.importDynamicPackages = importDynamicPackages;
+			ClassFileTransformer classFileTransformer,
+			String importDynamicPackages) {
+		this.classFileTransformer = classFileTransformer;
+		this.importDynamicPackages = importDynamicPackages;
 	}
 
 	public String getImportDynamicPackages() {
-	    return importDynamicPackages;
+		return importDynamicPackages;
 	}
 
 	@Override
 	public byte[] transform(ClassLoader loader, String className,
-		Class<?> classBeingRedefined,
-		ProtectionDomain protectionDomain, byte[] classfileBuffer)
-		throws IllegalClassFormatException {
-	    return classFileTransformer.transform(loader, className,
-		    classBeingRedefined, protectionDomain, classfileBuffer);
+			Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+			byte[] classfileBuffer) throws IllegalClassFormatException {
+		return classFileTransformer.transform(loader, className,
+				classBeingRedefined, protectionDomain, classfileBuffer);
 	}
 
 }
