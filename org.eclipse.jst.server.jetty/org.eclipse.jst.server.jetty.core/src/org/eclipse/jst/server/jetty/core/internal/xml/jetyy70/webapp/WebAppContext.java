cb64c2d06a35ccc7087ae9123634eaf05223a1d6
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-09-30 12:49:04 +0000

    Fix problem when context path is modified with UI.

--- org.eclipse.jst.server.jetty/org.eclipse.jst.server.jetty.core/src/org/eclipse/jst/server/jetty/core/internal/xml/jetyy70/webapp/WebAppContext.java
+++ org.eclipse.jst.server.jetty/org.eclipse.jst.server.jetty.core/src/org/eclipse/jst/server/jetty/core/internal/xml/jetyy70/webapp/WebAppContext.java
@@ -1,15 +1,24 @@
 package org.eclipse.jst.server.jetty.core.internal.xml.jetyy70.webapp;
 
+import java.io.IOException;
+
 import org.eclipse.jst.server.jetty.core.internal.xml.XMLElement;
+import org.eclipse.jst.server.jetty.core.internal.xml.XMLUtil;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 public class WebAppContext extends XMLElement {
 
+	private String fileName;
+
 	public void setContextPath(String contextPath) {
 		Element element = super.findElement("Set", "contextPath");
-		element.setTextContent("/" + contextPath);
+		if (contextPath.startsWith("/")) {
+			element.setTextContent(contextPath);
+		} else {
+			element.setTextContent("/" + contextPath);
+		}
 
 	}
 
@@ -29,4 +38,16 @@ public class WebAppContext extends XMLElement {
 		return element.getTextContent();
 	}
 
+	public String getWar() {
+		Element element = super.findElement("Set", "war");
+		return element.getTextContent();
+	}
+
+	public void save() throws IOException {
+		XMLUtil.save(fileName, getElementNode().getOwnerDocument());
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 }
