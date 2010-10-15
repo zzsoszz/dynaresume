141360c36c5030d99e4e9884f4eb228bbfc6aca4
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-15 16:23:08 +0000

    Initial Import for JM2T Acceleo2 UI.

---
+++ org.eclipse.gmt.modisco.jm2t/org.eclipse.gmt.modisco.jm2t.acceleo2.ui/src/org/eclipse/gmt/modisco/jm2t/acceleo2/internal/ui/JM2TAcceleo2UI.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Angelo ZERR.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:      
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gmt.modisco.jm2t.acceleo2.internal.ui;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class JM2TAcceleo2UI implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		JM2TAcceleo2UI.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		JM2TAcceleo2UI.context = null;
+	}
+
+}
