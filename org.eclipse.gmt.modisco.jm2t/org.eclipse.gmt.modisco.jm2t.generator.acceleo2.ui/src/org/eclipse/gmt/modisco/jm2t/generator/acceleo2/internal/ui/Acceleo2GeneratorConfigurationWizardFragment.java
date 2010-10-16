20af4b73a596218422d53cdf6b6099d64a139984
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-16 07:22:40 +0000

    Manage GeneratorType Extension Point.

---
+++ org.eclipse.gmt.modisco.jm2t/org.eclipse.gmt.modisco.jm2t.generator.acceleo2.ui/src/org/eclipse/gmt/modisco/jm2t/generator/acceleo2/internal/ui/Acceleo2GeneratorConfigurationWizardFragment.java
@@ -0,0 +1,7 @@
+package org.eclipse.gmt.modisco.jm2t.generator.acceleo2.internal.ui;
+
+import org.eclipse.gmt.modisco.jm2t.ui.wizard.WizardFragment;
+
+public class Acceleo2GeneratorConfigurationWizardFragment extends WizardFragment {
+
+}
