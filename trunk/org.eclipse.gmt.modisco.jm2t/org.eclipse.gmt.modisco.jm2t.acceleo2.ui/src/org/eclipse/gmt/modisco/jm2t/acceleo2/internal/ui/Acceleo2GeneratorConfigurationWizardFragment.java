b94af9ba087d89beb192f6015fde024965ff3dc0
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-16 07:19:12 +0000

    Rename to *generator.

--- trunk/org.eclipse.gmt.modisco.jm2t/org.eclipse.gmt.modisco.jm2t.acceleo2.ui/src/org/eclipse/gmt/modisco/jm2t/acceleo2/internal/ui/Acceleo2GeneratorConfigurationWizardFragment.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.gmt.modisco.jm2t.acceleo2.internal.ui;
-
-import org.eclipse.gmt.modisco.jm2t.ui.wizard.WizardFragment;
-
-public class Acceleo2GeneratorConfigurationWizardFragment extends WizardFragment {
-
-}
