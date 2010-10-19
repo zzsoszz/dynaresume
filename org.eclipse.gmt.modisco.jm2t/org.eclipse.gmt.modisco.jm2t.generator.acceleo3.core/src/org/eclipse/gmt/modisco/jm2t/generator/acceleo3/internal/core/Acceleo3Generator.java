b4342a814d7dcdf7cf2d7fa5e708109716cb3218
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-19 14:01:58 +0000

    Initialize JM2T/Acceleo3 Core project.

---
+++ org.eclipse.gmt.modisco.jm2t/org.eclipse.gmt.modisco.jm2t.generator.acceleo3.core/src/org/eclipse/gmt/modisco/jm2t/generator/acceleo3/internal/core/Acceleo3Generator.java
@@ -0,0 +1,14 @@
+package org.eclipse.gmt.modisco.jm2t.generator.acceleo3.internal.core;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmt.modisco.jm2t.core.generator.AbstractGenerator;
+import org.eclipse.gmt.modisco.jm2t.core.generator.IGeneratorConfiguration;
+
+public class Acceleo3Generator extends AbstractGenerator<EObject> {
+
+	public void generate(EObject model, IGeneratorConfiguration configuration) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
