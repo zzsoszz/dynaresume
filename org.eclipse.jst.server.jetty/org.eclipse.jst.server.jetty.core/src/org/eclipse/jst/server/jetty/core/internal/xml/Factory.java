847554b2c01cd24dad2badf0e23fae7d32020f5b
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-20 16:58:02 +0000

    Add EPL License.

--- org.eclipse.jst.server.jetty/org.eclipse.jst.server.jetty.core/src/org/eclipse/jst/server/jetty/core/internal/xml/Factory.java
+++ org.eclipse.jst.server.jetty/org.eclipse.jst.server.jetty.core/src/org/eclipse/jst/server/jetty/core/internal/xml/Factory.java
@@ -1,12 +1,13 @@
 /*******************************************************************************
- * Copyright (c) 2010 Angelo Zerr and others.
+ * Copyright (c) 2003, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Angelo Zerr <angelo.zerr@gmail.com> - Initial API and implementation 
+ *     IBM Corporation - Initial API and implementation
+ *     Angelo Zerr <angelo.zerr@gmail.com> - Jetty packages
  *******************************************************************************/
 package org.eclipse.jst.server.jetty.core.internal.xml;
 
