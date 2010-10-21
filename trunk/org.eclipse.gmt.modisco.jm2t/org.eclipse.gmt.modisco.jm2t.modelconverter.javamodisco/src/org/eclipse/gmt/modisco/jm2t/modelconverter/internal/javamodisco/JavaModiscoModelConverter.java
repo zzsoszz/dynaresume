abf3d847da724b43dd9eda2093049083a205f2ed
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-21 09:10:40 +0000

    Replace ModelProvider with ModelConverter.

---
+++ trunk/org.eclipse.gmt.modisco.jm2t/org.eclipse.gmt.modisco.jm2t.modelconverter.javamodisco/src/org/eclipse/gmt/modisco/jm2t/modelconverter/internal/javamodisco/JavaModiscoModelConverter.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Angelo ZERR.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:      
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gmt.modisco.jm2t.modelconverter.internal.javamodisco;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gmt.modisco.infra.discoverymanager.DiscoveryParameter;
+import org.eclipse.gmt.modisco.java.actions.DefaultDiscoverer;
+import org.eclipse.gmt.modisco.jm2t.core.generator.AbstractModelConverter;
+import org.eclipse.gmt.modisco.jm2t.core.generator.IGeneratorConfiguration;
+import org.eclipse.jdt.core.IJavaElement;
+
+/**
+ * JM2T Model converter to convert JDT JavaElement as EMF model with Java
+ * Modisco.
+ * 
+ */
+public class JavaModiscoModelConverter extends AbstractModelConverter {
+
+	public Object convert(Object model, IGeneratorConfiguration configuration) {
+		IJavaElement element = (IJavaElement) model;
+
+		// Get Modisco Discover switch the JDT JavaElement (IJavaProject,
+		// ICompilationUnit)
+		IExtendedDiscover discoverer = DiscovererFactory.getFactory()
+				.createDiscoverer(element);
+
+		// Initialize parameters of the discoverer
+		Map<DiscoveryParameter, Object> parameters = new HashMap<DiscoveryParameter, Object>();
+		parameters.put(DefaultDiscoverer.PARAMETER_SILENT_MODE, true);
+		parameters.put(DefaultDiscoverer.PARAMETER_BROWSE_RESULT, false);
+
+		// Transform JDT JavaElement to EMF Java Model Modisco instance
+		discoverer.discoverElement(discoverer.getSource(), parameters);
+
+		// Get and returns the result EMF model
+		Resource javaModel = (Resource) parameters
+				.get(DefaultDiscoverer.PARAMETER_TARGET_RESOURCE);
+		EObject emfObject = javaModel.getContents().get(0);
+		return emfObject;
+	}
+
+}
