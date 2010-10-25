534245c16729268d79e6e6e2a5b0c751ee446f29
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-25 15:20:21 +0000

    Initial import.

---
+++ trunk/org.eclipse.gmt.modisco.jm2t/org.eclipse.gmt.modisco.jm2t.modelconverter.javamodisco.core/src/org/eclipse/gmt/modisco/jm2t/modelconverter/javamodisco/internal/core/ExtendedDiscoverJavaModelFromJavaProject.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Angelo ZERR.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:      
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gmt.modisco.jm2t.modelconverter.javamodisco.internal.core;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.gmt.modisco.java.actions.DiscoverJavaModelFromJavaProject;
+import org.eclipse.jdt.core.IJavaProject;
+
+public class ExtendedDiscoverJavaModelFromJavaProject extends
+		DiscoverJavaModelFromJavaProject implements IExtendedDiscover {
+
+	private IJavaProject project;
+
+	public ExtendedDiscoverJavaModelFromJavaProject(IJavaProject project) {
+		this.project = project;
+	}
+
+	@Override
+	public void saveResource(URI target, IPath path, Resource resource,
+			IProgressMonitor monitor) throws IOException {
+		// Don't save EMF JavaModel as XMI file.
+	}
+
+	public Object getSource() {
+		return project;
+	}
+
+}
