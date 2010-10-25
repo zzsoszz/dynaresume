a296902836bfba3e24c3fa58747a5f47d5cd28e2
angelo.zerr <angelo.zerr@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2010-10-25 15:18:54 +0000

    Initial import.

---
+++ org.eclipse.gmt.modisco.jm2t/org.eclipse.gmt.modisco.jm2t.generator.jet.core/src/org/eclipse/gmt/modisco/jm2t/generator/jet/internal/core/JM2TJETCore.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Angelo ZERR.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:      
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.gmt.modisco.jm2t.generator.jet.internal.core;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class JM2TJETCore extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.gmt.modisco.jm2t.generator.jet.core"; //$NON-NLS-1$
+
+	// The shared instance
+	private static JM2TJETCore plugin;
+
+	/**
+	 * The constructor
+	 */
+	public JM2TJETCore() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext
+	 * )
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		plugin = this;
+		super.start(bundleContext);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		plugin = null;
+		super.stop(bundleContext);
+	}
+
+	/**
+	 * Returns the shared instance
+	 * 
+	 * @return the shared instance
+	 */
+	public static JM2TJETCore getDefault() {
+		return plugin;
+	}
+
+}
