6d082bd73ea9fe9652b933660d7d07f16b9a411d
pascal.leclercq@gmail.com <pascal.leclercq@gmail.com@9c1fdf9a-ce3e-11de-bd98-e341d93cc3cc>
2011-06-09 21:04:41 +0000

    org.hibernate.transaction.OSGiTransactionManagerLookup

---
+++ trunk/infrastructure/org.dynaresume.hibernate.atomikos.adapter/src/main/java/org/hibernate/transaction/OSGiTransactionManagerLookup.java
@@ -0,0 +1,31 @@
+package org.hibernate.transaction;
+
+import java.util.Properties;
+
+import javax.transaction.Transaction;
+import javax.transaction.TransactionManager;
+
+import org.hibernate.HibernateException;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+
+public class OSGiTransactionManagerLookup implements TransactionManagerLookup {
+
+	public TransactionManager getTransactionManager(Properties props) throws HibernateException {
+		Bundle hostBundle = FrameworkUtil.getBundle(TransactionManager.class);
+		ServiceReference ref = hostBundle.getBundleContext().getServiceReference(TransactionManager.class.getName());
+		return (TransactionManager) hostBundle.getBundleContext().getService(ref);
+
+	}
+
+	public String getUserTransactionName() {
+		return TransactionManager.class.getName();
+	}
+
+	// new in Hibernate 3.3
+	public Object getTransactionIdentifier(Transaction transaction) {
+		return transaction;
+	}
+
+}
